acb3fa11ca3ab000893e8c8e1064b46ba37245c1
Luke Skarth-Hayley <lukeskt@users.noreply.github.com>
2021-09-07 10:05:42 +0100

    Add scripts and prefab plus unitypackage

---
+++ InputSystemFirstPersonCharacterScripts/InputSystemFirstPersonCharacter.cs
@@ -0,0 +1,146 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class InputSystemFirstPersonCharacter : MonoBehaviour
+{
+    private InputSystemFirstPersonControls inputActions;
+
+    private CharacterController controller;
+    
+    [SerializeField] private Camera cam;
+    [SerializeField] private float movementSpeed = 2.0f;
+    [SerializeField] public float lookSensitivity = 1.0f;
+    
+    private float xRotation = 0f;
+
+    // Movement Vars
+    private Vector3 velocity;
+    public float gravity = -9.81f;
+    private bool grounded;
+
+    // Zoom Vars - Zoom code adapted from @torahhorse's First Person Drifter scripts.
+    public float zoomFOV = 30.0f;
+    public float zoomSpeed = 9f;
+    private float targetFOV;
+    private float baseFOV;
+
+    // Crouch Vars
+    private float initHeight;
+    [SerializeField] private float crouchHeight;
+
+    private void Awake()
+    {
+        inputActions = new InputSystemFirstPersonControls();
+    }
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        initHeight = controller.height;
+        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+        SetBaseFOV(cam.fieldOfView);
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void Update()
+    {
+        DoLooking();
+        DoMovement();
+        DoZoom();
+        //DoCrouch();
+    }
+
+    private void DoLooking()
+    {
+        Vector2 looking = GetPlayerLook();
+        float mouseX = looking.x * lookSensitivity * Time.deltaTime;
+        float mouseY = looking.y * lookSensitivity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+
+    private void DoMovement()
+    {
+        grounded = controller.isGrounded;
+        if (grounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        Vector2 movement = GetPlayerMovement();
+        Vector3 move = transform.right * movement.x + transform.forward * movement.y;
+        controller.Move(move * movementSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void DoZoom()
+    {
+        if (inputActions.FPSController.Zoom.ReadValue<float>() > 0)
+        {
+            targetFOV = zoomFOV;
+        }
+        else
+        {
+            targetFOV = baseFOV;
+        }
+        UpdateZoom();
+    }
+
+    private void DoCrouch()
+    {
+        // TODO: check if underneath an object and stop at collision
+        if (inputActions.FPSController.Crouch.ReadValue<float>() > 0)
+        {
+            controller.height = crouchHeight;
+        }
+        else
+        {
+            // this doesn't work to check collision...
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), 0.5f))
+            {
+                controller.height = crouchHeight;
+            }
+            else
+            {
+                controller.height = initHeight;
+            }
+        }
+    }
+
+    private void UpdateZoom()
+    {
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, targetFOV, zoomSpeed * Time.deltaTime);
+    }
+
+    public void SetBaseFOV(float fov)
+    {
+        baseFOV = fov;
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    public Vector2 GetPlayerMovement()
+    {
+        return inputActions.FPSController.Move.ReadValue<Vector2>();
+    }
+
+    public Vector2 GetPlayerLook()
+    {
+        return inputActions.FPSController.Look.ReadValue<Vector2>();
+    }
+}
